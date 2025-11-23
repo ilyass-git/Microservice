8e195ba4080e7418a763ad1b34e9e89d1296db05
ilyass <ilyasmachmoum@gmail.com>
2025-11-23 12:57:44 +0100

    eureka

---
+++ Desktop/projects/CollocationMicroservice/src/main/java/emsi/ma/collocationmicroservice/CollocationMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package emsi.ma.collocationmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollocationMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollocationMicroserviceApplication.class, args);
+    }
+
+}
