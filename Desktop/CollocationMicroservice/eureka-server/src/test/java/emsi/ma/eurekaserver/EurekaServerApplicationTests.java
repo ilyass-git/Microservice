b1698cf8ca070a2780810ec5f9ab96d8d6187ddc
ilyass <ilyasmachmoum@gmail.com>
2025-11-23 18:17:28 +0100

    micro services communication nedded

---
+++ Desktop/CollocationMicroservice/eureka-server/src/test/java/emsi/ma/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package emsi.ma.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
